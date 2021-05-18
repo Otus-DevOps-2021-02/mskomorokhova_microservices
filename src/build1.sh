d15f4c56511b982bbe19e5baf9ac2247cae9a9fd
Marina Skomorokhova <mskomorokhova@diasoft.ru>
2021-05-18 20:41:27 +0300

    upd build

--- src/build1.sh
+++ src/build1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker pull mongo:latest
-docker build -t $USER/post:1.0 ./post-py
-docker build -t $USER/comment:1.0 ./comment
-docker build -t $USER/ui:2.0 ./ui
+docker network create reddit
+docker volume create reddit_db
+docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db mongo:latest
+docker run -d --network=reddit --network-alias=post $USER/post:1.0
+docker run -d --network=reddit --network-alias=comment $USER/comment:1.0
+docker run -d --network=reddit -p 9292:9292 $USER/ui:2.0
